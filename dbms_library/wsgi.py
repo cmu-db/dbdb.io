7cf0c32351620d2908af5f39015e5d1e04e16167
Andy Pavlo <pavlo@cs.brown.edu>
2014-05-16 13:05:22 -0400

    Checkpoint from claudius

---
+++ dbms_library/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for dbms_library project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbms_library.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
