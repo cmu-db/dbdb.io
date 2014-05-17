394dd34449cf0e6690bf6b6e7d63530e38d07c20
Andy Pavlo <pavlo@cs.brown.edu>
2014-05-17 19:50:06 -0400

    Initial data fixtures

--- dbms_library/systems/admin.py
+++ dbms_library/systems/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from dbms_library.systems.models import *
+
 # Register your models here.
+admin.site.register(OperatingSystem)
+admin.site.register(ProgrammingLanguage)
+admin.site.register(System)
--- dbms_library/systems/models.py
+++ dbms_library/systems/models.py
@@ -2,12 +2,27 @@ from django.db import models
 
 class OperatingSystem(models.Model):
     name = models.CharField(max_length=16)
-    website = models.URLField(default=None)
+    website = models.URLField(default=None, null=True)
+    
+    def __unicode__(self):
+        return self.name
+# CLASS
 
 class ProgrammingLanguage(models.Model):
     name = models.CharField(max_length=32)
-    website = models.URLField(default=None)
+    website = models.URLField(default=None, null=True)
+    
+    def __unicode__(self):
+        return self.name
+# CLASS
 
+class Publication(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.CharField(max_length=255)
+    bibtex = models.TextField(default=None, null=True)
+    download = models.URLField(default=None, null=True)
+    year = models.IntegerField()
+# CLASS
 
 class System(models.Model):
     COMMERCIAL_STATUS_TYPE = (
@@ -18,12 +33,13 @@ class System(models.Model):
     
     name = models.CharField(max_length=64)
     description = models.TextField()
-    website = models.URLField(default=None)
+    website = models.URLField(default=None, null=True)
     written_in = models.ManyToManyField(ProgrammingLanguage, related_name='wi+')
     oses = models.ManyToManyField(OperatingSystem, related_name='os+')
-    commercial_status = models.CharField(max_length=1, choices=COMMERCIAL_STATUS_TYPE, default=None)
-    start_year = models.IntegerField(default=None)
-    end_year = models.IntegerField(default=None)
+    publications = models.ManyToManyField(Publication, related_name='p+')
+    commercial_status = models.CharField(max_length=1, choices=COMMERCIAL_STATUS_TYPE, default=None, null=True)
+    start_year = models.IntegerField(default=None, null=True)
+    end_year = models.IntegerField(default=None, null=True)
     
     # Features
     support_sql = models.BooleanField(default=False)
@@ -35,3 +51,6 @@ class System(models.Model):
     support_triggers = models.BooleanField(default=False)
     support_languages = models.ManyToManyField(ProgrammingLanguage, related_name='+l')
     
+    def __unicode__(self):
+        return self.name
+# CLASS
