644cf85b014ca2718fc4ce958067e494368e46a1
Rohan Varma <rnvarma@db.cs.cmu.edu>
2015-02-11 15:27:35 -0500

    Hack to get wsgi working...

--- dbms_library/wsgi.py
+++ dbms_library/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbms_library.settings")
+import os, sys
+
+sys.path.append('/var/www/databaseology')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "dbms_library.settings"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbms_library.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
