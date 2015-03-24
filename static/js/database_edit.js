cf9fbfb129862de2b5afc806fa955c76e421aed2
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-03-24 16:59:59 -0400

    markup support finalized

--- static/js/database_edit.js
+++ static/js/database_edit.js
@@ -16,6 +16,7 @@ function close_text_area($elem, text) {
   }
   $elem.empty();
   $elem.text(text);
+  $elem.next().text(text);
 }
 
 function open_text_area($elem) {
@@ -25,7 +26,7 @@ function open_text_area($elem) {
   $(".save-button").show(500);
   $elem.addClass("edited");
   $elem.addClass("editing");
-  var value = $elem.text().trim();
+  var value = $elem.next().text().trim();
   last_saved_textarea_property = value;
   $elem.empty();
   var textarea = document.createElement("textarea");
--- systems/views.py
+++ systems/views.py
@@ -86,6 +86,17 @@ class LoadContext(object):
     return db
 
   @staticmethod
+  def load_db_raw_markdown_fields(db_data, db_obj):
+    for field in db_obj.__dict__:
+      if "rendered" in field:
+        fieldName = field[1:-9]
+        if db_obj.__getattribute__(fieldName):
+          rawField = db_obj.__getattribute__(fieldName).raw
+          db_data[fieldName + "_raw"] = rawField
+        else:
+          db_data[fieldName + "_raw"] = ""
+
+  @staticmethod
   def get_fields(db):
     field_supports = []
     for field in db: 
@@ -237,6 +248,7 @@ class DatabaseEditingPage(View):
     if database.secret_key == key:
       context = LoadContext.load_base_context(request)
       context["db"] = LoadContext.load_db_data(SystemSerializer(database).data)
+      LoadContext.load_db_raw_markdown_fields(context["db"], database)
       return render(request, 'database_edit.html',
         context)
     else:
