e56241524a7f18d9e8128a36ddb3e5b1a03bcfd7
Andy Pavlo <pavlo@cs.brown.edu>
2016-07-04 09:28:39 -0400

    Fixes for Django 1.9 on production server

--- website/urls.py
+++ website/urls.py
@@ -6,10 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^', include('systems.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- website/wsgi.py
+++ website/wsgi.py
@@ -2,18 +2,12 @@
 WSGI config for DBDB.IO project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os, sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(os.path.join(BASE_DIR, "..")))
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "website.settings"
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
