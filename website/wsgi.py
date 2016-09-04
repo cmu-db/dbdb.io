d9017eaa49243bbf810e91e61aad85cf59f7c94e
Andy Pavlo <pavlo@cs.brown.edu>
2016-09-04 13:33:38 -0400

    Fixed wsgi.py to work correctly in Apache with multiple django sites

--- website/wsgi.py
+++ website/wsgi.py
@@ -7,10 +7,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
+# Change the env variable where django looks for the settings module
+# http://stackoverflow.com/a/11817088
+import django.conf
+django.conf.ENVIRONMENT_VARIABLE = "DJANGO_DBDBIO_SETTINGS_MODULE"
+os.environ.setdefault("DJANGO_DBDBIO_SETTINGS_MODULE", "website.settings")
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
