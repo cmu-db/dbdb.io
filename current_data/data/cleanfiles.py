d6dd40ec1f9d6226b23810c1c1ed0bfb755742dd
Femi Olukoya <oolukoya@andrew.cmu.edu>
2017-04-15 23:04:58 -0400

    Restructure current_data directory for new scripts

--- current_data/data/cleanfiles.py
+++
@@ -1,9 +0,0 @@
-import os
-
-files = os.listdir"."
-for f in files:
-    with openf, "r" as infile, openf + "-out.txt", "w" as outfile:
-        for line in infile:
-            outfile.writeline.replace"", ""
-                              .replace"", ""
-                              .replace""", """
--- current_data/parse_csv.py
+++ current_data/parse_csv.py
@@ -1,7 +1,9 @@
 import csv
 import json
+import sys
 
-from StringIO import StringIO
+directory = 'json_data/'
+sys.path.append(directory)
 
 with open('systems.csv', 'r') as input_file:
   reader = csv.reader(input_file)
@@ -11,10 +13,11 @@ with open('systems.csv', 'r') as input_file:
 
     if name:
       try:
-        with open(name + '.json', 'w') as output_file:
+        with open(directory + name + '.json', 'w') as output_file:
           output_file.write(json.dumps({
             'Name': name,
             'Website': website
-          }))
+          },
+            indent=4))
       except Exception as e:
         print e
