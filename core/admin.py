b75276768c779803d262d4fe9fa396ebe3db2255
Andy Pavlo <pavlo@cs.brown.edu>
2017-12-08 18:44:25 -0500

    Added back SystemAdmin

--- core/admin.py
+++ core/admin.py
@@ -10,6 +10,12 @@ class FeatureOptionsInlines(admin.StackedInline):
 
 class FeatureAdmin(admin.ModelAdmin):
     inlines = [FeatureOptionsInlines]
+    
+class SystemAdmin(admin.ModelAdmin):
+    empty_value_display = 'unknown'
+    list_display = ('name', 'created', 'current_version',)
+    list_filter = ['created' ]
+    search_fields = ('name', )
 
 
 admin.site.register(OperatingSystem)
@@ -20,7 +26,7 @@ admin.site.register(Publication)
 admin.site.register(Feature, FeatureAdmin)
 admin.site.register(FeatureOption)
 admin.site.register(SuggestedSystem)
-admin.site.register(System)
+admin.site.register(System, SystemAdmin)
 admin.site.register(SystemVersion)
 admin.site.register(SystemVersionMetadata)
 admin.site.register(SystemFeatures)
