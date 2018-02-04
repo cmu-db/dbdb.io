2d9dfcd05d09db2520d8893e78ef5518a9fa75b2
Andy Pavlo <pavlo@cs.brown.edu>
2018-02-03 22:20:25 -0500

    Moved settings.py back so that we can run tests properly

--- dbdb_io/settings.py
+++ dbdb_io/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*** CHANGE ME ***'
+SECRET_KEY = 'u0e4p!jx40iqba82rhnke4#o9c(oz4h5l$h5e3qz550^igd*sj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
