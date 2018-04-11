d36294f821e3806910008fedb6f5b605a62c0b29
Andy Pavlo <pavlo@cs.brown.edu>
2018-04-11 09:15:59 -0400

    Fix for wsgi.py running on Apache

--- dbdb/wsgi.py
+++ dbdb/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+
+# Change the env variable where django looks for the settings module
+# http://stackoverflow.com/a/11817088
+import django.conf
+django.conf.ENVIRONMENT_VARIABLE = "DJANGO_DBDBIO_SETTINGS_MODULE"
+os.environ.setdefault("DJANGO_DBDBIO_SETTINGS_MODULE", "dbdb.settings")
+sys.path.append(base_dir)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbdb.settings")
-
 application = get_wsgi_application()
