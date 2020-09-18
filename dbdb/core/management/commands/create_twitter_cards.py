91f8cbee3416ff47fc62adcd5b2bbf734226df50
Andy Pavlo <pavlo@cs.brown.edu>
2020-09-17 21:27:23 -0400

    Improved twitter card script so that I can run it in cron

--- dbdb/core/management/commands/create_twitter_cards.py
+++ dbdb/core/management/commands/create_twitter_cards.py
@@ -1,5 +1,6 @@
 # stdlib imports
 import os
+import sys
 
 # django imports
 from django.core.management import BaseCommand
@@ -16,8 +17,8 @@ from dbdb.core.views import EmptyFieldsView
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--system', type=str)
-        parser.add_argument('--missing', action='store_true')
+        parser.add_argument('system', metavar='S', type=str, nargs='?',
+                    help='System to force twiter card creation')
         return
 
     def handle(self, *args, **options):
@@ -25,20 +26,32 @@ class Command(BaseCommand):
         assert os.path.exists(template), "Missing: " + template
 
         versions = SystemVersion.objects.filter(is_current=True)
+        force = False
         if options['system']:
             keyword = options['system']
-
             if keyword.isdigit():
                 versions = versions.filter(system__id=int(keyword))
             else:
                 versions = versions.filter(system__name__icontains=keyword)
+            force = True
 
         for ver in versions:
             card_img = os.path.join(settings.TWITTER_CARD_ROOT, ver.get_twitter_card_image())
-            if options['missing'] and os.path.exists(card_img): continue
+            logo_img = os.path.join(settings.MEDIA_ROOT, ver.logo.name)
+            
+            # Only create ones that are missing or where the logo 
+            # is newer than the existing twitter image
+            if not force:
+                if os.path.exists(card_img) and os.path.exists(logo_img):
+                    if os.path.getmtime(logo_img) <= os.path.getmtime(card_img):
+                        #print(logo_img, "=>", os.path.getmtime(logo_img))
+                        #print(card_img, "=>", os.path.getmtime(card_img))
+                        #self.stdout.write("SKIP: %s -> %s" % (ver.system.name, card_img))
+                        continue
+                elif os.path.exists(card_img):
+                    continue
             ver.create_twitter_card()
             self.stdout.write("%s -> %s" % (ver.system.name, card_img))
-
         # FOR
         return
 
