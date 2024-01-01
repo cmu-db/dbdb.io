6e9dbe20dab8b763a037652105fdb1e6ca1a23d7
Andy Pavlo <pavlo@cs.brown.edu>
2024-01-01 11:10:43 -0500

    Merging from master

--- dbdb/core/migrations/0035_systemversion_linkedin_url.py
+++ dbdb/core/migrations/0035_systemversion_linkedin_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0032_systemsearchtext_name'),
+        ('core', '0034_alter_citationurl_url'),
     ]
 
     operations = [
