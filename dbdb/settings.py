31f225c78abc492076a3212b2786321419e28685
Andy Pavlo <pavlo@cs.brown.edu>
2025-06-01 21:34:58 -0400

    Disable SQL debug log output on by default

--- dbdb/settings.py
+++ dbdb/settings.py
@@ -77,27 +77,27 @@ ROOT_URLCONF = 'dbdb.urls'
 WSGI_APPLICATION = 'dbdb.wsgi.application'
 
 # Uncommenting this will enable query logging to stdout
-LOGGING = {
-   'version': 1,
-   'filters': {
-       'require_debug_true': {
-           '()': 'django.utils.log.RequireDebugTrue',
-       }
-   },
-   'handlers': {
-       'console': {
-           'level': 'DEBUG',
-           'filters': ['require_debug_true'],
-           'class': 'logging.StreamHandler',
-       }
-   },
-   'loggers': {
-       'django.db.backends': {
-           'level': 'DEBUG',
-           'handlers': ['console'],
-       }
-   }
-}
+# LOGGING = {
+#    'version': 1,
+#    'filters': {
+#        'require_debug_true': {
+#            '()': 'django.utils.log.RequireDebugTrue',
+#        }
+#    },
+#    'handlers': {
+#        'console': {
+#            'level': 'DEBUG',
+#            'filters': ['require_debug_true'],
+#            'class': 'logging.StreamHandler',
+#        }
+#    },
+#    'loggers': {
+#        'django.db.backends': {
+#            'level': 'DEBUG',
+#            'handlers': ['console'],
+#        }
+#    }
+# }
 
 
 # Database
