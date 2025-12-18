af3c53afba69afc175582e690f055b137e0d859b
Andy Pavlo <pavlo@cs.brown.edu>
2025-12-17 20:25:50 -0500

    Added template tags for generating CitationUrl links with accompanying meta-data

--- dbdb/core/templatetags/citations.py
+++ dbdb/core/templatetags/citations.py
@@ -1,10 +1,19 @@
 from django import template
 
+from dbdb.core.models import CitationUrl
+
 register = template.Library()
 
-@register.inclusion_tag("components/citation_link.html")
-def citation_link(number    ):
+@register.inclusion_tag("components/citation_footnote.html")
+def citation_footnote(number: int):
     """
-    Renders a citation <a> tag given a citation number
+    Renders a citation footnote <a> tag given a citation number
     """
     return {"number": number}
+
+@register.inclusion_tag("components/citation_link.html")
+def citation_link(citation: CitationUrl):
+    """
+    Renders a citation link for a CitationUrl
+    """
+    return {"c": citation, "Status": CitationUrl.Status}
